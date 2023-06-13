cb27cc2cbeadd1131e38a7dcbc3c16ef20fa4f91
Robert Reris <rreris@fortinet.com>
2023-06-13 08:44:16 -0700

    local_copy script updates

--- scripts/local_copy.sh
+++ scripts/local_copy.sh
@@ -1,3 +1,24 @@
+#!/bin/bash
+
 cp ../UserRepo/layouts/shortcodes/* layouts/shortcodes
 cp ../UserRepo/layouts/partials/* layouts/partials
-hugo server --bind=0.0.0.0
+
+case "$1" in
+  "server")
+    cmd="hugo server --bind=0.0.0.0"
+    ;;
+
+  "shell")
+    cmd="/bin/bash"
+    ;;
+
+  "build" | "")
+    cmd="hugo --minify --cleanDestinationDir"
+    ;;
+
+  *)
+    cmd=""
+    ;;
+esac
+
+$cmd
