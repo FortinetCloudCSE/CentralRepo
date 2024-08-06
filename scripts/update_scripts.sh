05021331427d39f4e6d008aa6d3671da99b125ed
Jeff Kopko <jkopko@gmail.com>
2024-08-06 12:50:21 -0700

    add github action to update_scripts

--- scripts/update_scripts.sh
+++ scripts/update_scripts.sh
@@ -2,10 +2,10 @@
 
 cp scripts/docker_tester_run.sh ../UserRepo/scripts/docker_tester_run.sh
 cp scripts/docker_run.sh ../UserRepo/scripts/docker_run.sh
-
+cp scripts/static.yaml ../UserRepo/.github/workflows/static.yaml
 cp Dockerfile ../UserRepo/Dockerfile
 
-echo "updated docker_tester_run.sh, docker_run.sh, and Dockerfile"
+echo "updated docker_tester_run.sh, docker_run.sh, Github action, and Dockerfile"
 
 if ! test -f ../UserRepo/scripts/repoConfig.json; then
   cp scripts/repoConfig.json ../UserRepo/scripts/repoConfig.json
