8a638242922d5f37b5675968a83c7ff4eaae2aef
Jeff Kopko <jkopko@gmail.com>
2024-08-06 13:48:28 -0700

    fix yml misspell in update_scripts

--- scripts/update_scripts.sh
+++ scripts/update_scripts.sh
@@ -2,7 +2,7 @@
 
 cp scripts/docker_tester_run.sh ../UserRepo/scripts/docker_tester_run.sh
 cp scripts/docker_run.sh ../UserRepo/scripts/docker_run.sh
-cp scripts/static.yaml ../UserRepo/.github/workflows/static.yaml
+cp scripts/static.yml ../UserRepo/.github/workflows/static.yml
 cp Dockerfile ../UserRepo/Dockerfile
 
 echo "updated docker_tester_run.sh, docker_run.sh, Github action, and Dockerfile"
