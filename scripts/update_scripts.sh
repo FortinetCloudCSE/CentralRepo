cdcec94c428af466a187371f1168381b3aa20dbf
Jeff Kopko <jkopko@gmail.com>
2024-08-19 12:36:42 -0400

    add venv/ to .gitignore from update_scripts

--- scripts/update_scripts.sh
+++ scripts/update_scripts.sh
@@ -7,6 +7,8 @@ cp Dockerfile ../UserRepo/Dockerfile
 
 echo "updated docker_tester_run.sh, docker_run.sh, Github action, and Dockerfile"
 
+echo "venv/" >> .gitignore
+
 if ! test -f ../UserRepo/scripts/repoConfig.json; then
   cp scripts/repoConfig.json ../UserRepo/scripts/repoConfig.json
   echo "copied Hugo config file into ../UserRepo/scripts/repoConfig.json"
