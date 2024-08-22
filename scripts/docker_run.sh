d37f17d9d28161f856fab6924d8b2705fa76e633
Jeff Kopko <jkopko@gmail.com>
2024-08-22 11:02:11 -0700

    adjust run commands for hugo or other scripts

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -5,10 +5,24 @@ myarray=( "build" "server" "shell" "generate_toml" "update_scripts" "update_fdev
 
 [[ "$#" > "1" ]] || [[ ! " ${myarray[*]} " =~ " $1 " ]] && echo "Usage: ./scripts/docker_run.sh [ build | server | generate_toml | update_scripts | update_fdevsec | shell ]" && exit 1
 
-cmd="docker run --rm -it
-  -v $(pwd):/home/UserRepo
-  --mount type=bind,source=$(pwd)/hugo.toml,target=/home/CentralRepo/hugo.toml
-  -p 1313:1313 fortinet-hugo:latest $1"
+case "$1" in
+  "server" | "shell" | "build" )
+    cmd="docker run --rm -it
+      -v $(pwd):/home/UserRepo
+      --mount type=bind,source=$(pwd)/hugo.toml,target=/home/CentralRepo/hugo.toml
+      -p 1313:1313 fortinet-hugo:latest $1"
+    ;;
+
+  "generate_toml" | "update_scripts" | "update_fdevsec")
+    cmd="docker run --rm -it
+    -v $(pwd):/home/UserRepo
+    fortinet-hugo:latest $1"
+    ;;
+
+  *)
+    cmd=""
+    ;;
+esac
 
 echo "**** Here's the docker run command we're using:   $cmd ****"
 $cmd
