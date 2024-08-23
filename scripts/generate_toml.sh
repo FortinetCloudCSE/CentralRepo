a10c9c68e8c18e7449a0e2d48d2dc1d833333484
Jeff Kopko <jkopko@gmail.com>
2024-08-23 12:03:39 -0700

    chagne python venv to use Container FS

--- scripts/generate_toml.sh
+++ scripts/generate_toml.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 echo "Activating Python venv and installing Jinja2 from pip"
-python -m venv /home/UserRepo/venv
-. /home/UserRepo/venv/bin/activate
+python -m venv /home/CentralRepo/venv
+. /home/CentralRepo/venv/bin/activate
 pip install Jinja2
 
 echo "Running generate_toml.py which uses Jinja2 template to generate new TOML file from repoConfig.json"
--- scripts/update_scripts.sh
+++ scripts/update_scripts.sh
@@ -7,8 +7,6 @@ cp Dockerfile ../UserRepo/Dockerfile
 
 echo "updated docker_tester_run.sh, docker_run.sh, Github action, and Dockerfile"
 
-echo "venv/" >> ../UserRepo/.gitignore
-
 if ! test -f ../UserRepo/scripts/repoConfig.json; then
   cp scripts/repoConfig.json ../UserRepo/scripts/repoConfig.json
   echo "copied Hugo config file into ../UserRepo/scripts/repoConfig.json"
