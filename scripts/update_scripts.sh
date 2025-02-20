8ac0ce134ccf2805e466e742a4bad343cfe30643
Jeff Kopko <jkopko@fortinet.com>
2025-02-20 20:33:59 +0000

    update_scripts: add copy of docker_build.sh and update_scripts

--- scripts/update_scripts.sh
+++ scripts/update_scripts.sh
@@ -2,21 +2,24 @@
 
 ### After QA testing docker_run_latest and docker_build_latest.sh: rm docker_tester_run, docker_run, docker_tester_build, docker_build
 
-cp scripts/docker_run_latest.sh ../UserRepo/scripts/docker_run_latest.sh
-cp scripts/docker_build_latest.sh ../UserRepo/scripts/docker_build_latest.sh
+## leave the wget (latest) scripts out b/c they have too much potential for compromise
+#cp scripts/docker_run_latest.sh ../UserRepo/scripts/docker_run_latest.sh
+#cp scripts/docker_build_latest.sh ../UserRepo/scripts/docker_build_latest.sh
 
 cp scripts/docker_tester_run.sh ../UserRepo/scripts/docker_tester_run.sh
 cp scripts/docker_tester_build.sh ../UserRepo/scripts/docker_tester_build.sh
 cp scripts/docker_run.sh ../UserRepo/scripts/docker_run.sh
-
+cp scripts/docker_build.sh ../UserRepo/scripts/docker_build.sh
 
 cp scripts/static.yml ../UserRepo/.github/workflows/static.yml
 cp Dockerfile ../UserRepo/Dockerfile
 
 
-echo "updated docker_tester_run.sh, docker_run.sh, Github action, and Dockerfile"
+echo "updated docker_tester_run.sh, docker_run.sh, docker_build.sh, Github action (static.yaml), and Dockerfile"
 
 if ! test -f ../UserRepo/scripts/repoConfig.json; then
   cp scripts/repoConfig.json ../UserRepo/scripts/repoConfig.json
   echo "copied Hugo config file into ../UserRepo/scripts/repoConfig.json"
 fi
+
+cp scripts/update_scripts.sh ../UserRepo/scripts/update_scripts.sh
