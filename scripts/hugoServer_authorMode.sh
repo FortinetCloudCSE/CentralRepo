dc972751746bddc49de22e3146293d587f93f1d3
Jeff Kopko <jkopko@fortinet.com>
2025-03-10 20:05:04 +0000

    REMOVE due to RACE CONDITION...Force generate_toml in server/author mode

--- scripts/hugoServer_authorMode.sh
+++ scripts/hugoServer_authorMode.sh
@@ -2,5 +2,5 @@
 
 rm /home/CentralRepo/layouts/partials/google_analytics.html
 mv /home/CentralRepo/layouts/partials/google_analytics_authorMode.html /home/CentralRepo/layouts/partials/google_analytics.html
-./scripts/generate_toml.sh
+# ./scripts/generate_toml.sh
 hugo server --contentDir /home/UserRepo/content --bind=0.0.0.0
